561e71afc24921e8b39ef79937d8b2f8caf100ef
stellaluong <57662112+stellaluong@users.noreply.github.com>
2020-03-04 12:34:06 +0100

    Initial commit

---
+++ Hva gjør denne opgaven/Hva gjør denne opgaven/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Hva_gjør_denne_opgaven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = 250;
+            var counts = new int[range];
+            int allLetters = 0;
+            string text = "something";
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine() ?? string.Empty;
+                foreach (var character in text.ToUpper())
+                {
+                    allLetters++;
+                    counts[character]++;
+                }
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {
+                        var character = (char)i;
+                        double percentage = 100 * counts[i] / allLetters;
+                        string show = character + " - " + percentage.ToString("F2") + "%";
+                        Console.CursorLeft = Console.BufferWidth - show.Length - 1;
+                        Console.WriteLine(show);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Passordgenerator/Passordgenerator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static readonly Random Random = new Random();
+        static void Main(string[] args)
+        {
+            if (!IsValid(args))
+            {
+                ShowHelpText();
+                return;
+            }
+            var length = Convert.ToInt32(args[0]);
+            var options = args[1];
+            var pattern = options.PadRight(length, 'l');
+            var password = string.Empty;
+            while (pattern.Length > 0)
+            {
+                var randomIndex = Random.Next(0, pattern.Length - 1);
+                var category = pattern[randomIndex];
+                pattern = pattern.Substring(0, randomIndex)
+                          + pattern.Substring(randomIndex + 1, pattern.Length - randomIndex - 1);
+
+                if (category == 'l') password += RandomLowerCase();
+                else if (category == 'L') password += RandomUpperCase();
+                else if (category == 'd') password += RandomDigit();
+                else password += RandomSpecialCharacter();
+            }
+            Console.WriteLine(password);
+              }
+              
+             static char RandomSpecialCharacter()
+            {
+                var allSpecialCharacter = "!\"#¤%&/(){}[]";
+                var index = Random.Next(0, allSpecialCharacter.Length - 1);
+                return allSpecialCharacter[index];
+            }
+             static char RandomDigit()
+            {
+                return Random.Next(0, 9).ToString()[0];
+            }
+
+        static char RandomUpperCase()
+            {
+                return GetRandomLetter('A', 'Z');
+            }
+            static char RandomLowerCase()
+            {
+                return GetRandomLetter('a', 'z');
+            }
+            static char GetRandomLetter(char min, char max)
+            {
+                return (char)Random.Next(min, max);
+            }
+            static bool IsValid(string[] args)
+            {
+                if (args.Length != 2) return false;
+
+                var lengthStr = args[0];
+                var options = args[1];
+            if (options.Any(character => character != 'l'
+                                         && character != 'L'
+                                         && character != 'd'
+                                         && character != 's'))
+            {
+                return false;
+            }
+            return lengthStr.All(char.IsDigit);
+            }
+            private static void ShowHelpText()
+            {
+                Console.WriteLine("PasswordGenerator <length> <options>");
+                Console.WriteLine("  Options:");
+                Console.WriteLine("  - l = lower case letter");
+                Console.WriteLine("  - L = upper case letter");
+                Console.WriteLine("  - d = digit");
+                Console.WriteLine("  - s = special character (!\"#¤%&/(){}[]");
+                Console.WriteLine("Example: PasswordGenerator 14 lLssdd");
+                Console.WriteLine("         Min. 1 lower case");
+                Console.WriteLine("         Min. 1 upper case");
+                Console.WriteLine("         Min. 2 special characters");
+                Console.WriteLine("         Min. 2 digits");
+            }
+        }
+        
+    }
+
